246bab8f704c1cb2e3326d4e9acac158609d97ef
anit-george <iamforgetful2003@gmail.com>
2024-06-30 16:59:46 +0530

    Add files via upload

--- src/Constants/Constants.js
+++ src/Constants/Constants.js
@@ -1,3 +1,5 @@
 export const baseUrl = "https://api.themoviedb.org/3";
 export const API_KEY = "b16034021d01dbc41a4b924b6e99babe";
 export const imageUrl = "https://image.tmdb.org/t/p/original";
+export const Bearer_token =
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMTYwMzQwMjFkMDFkYmM0MWE0YjkyNGI2ZTk5YmFiZSIsIm5iZiI6MTcxOTc0MjM0Ni4zMTU1NTMsInN1YiI6IjY2NzUxMGRlMmZiOGJiYTI5NjE2ZTE0MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.G9eAEZGWFgcY-inLUVLabL_XHu0nJHgj6hiGOCArwvk";
